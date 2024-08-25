5379caec177f644a311ef58d5160a277d0dea012
lilPupich <yuran1337228@gmail.com>
2024-08-25 05:11:27 +0300

    Первый коммит

---
+++ cmd/URLShortener/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"URLShortener/internal/config"
+	"fmt"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server:
+
+}
