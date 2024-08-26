0960ab6e87a5b5ae82364232fcf8c1b3abce9cc3
lilPupich <yuran1337228@gmail.com>
2024-08-26 04:39:03 +0300

    ready-made logger added

--- cmd/URLShortener/main.go
+++ cmd/URLShortener/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"URLShortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
 
 	// TODO: init storage: sqlite
 
@@ -15,4 +23,25 @@ func main() {
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
